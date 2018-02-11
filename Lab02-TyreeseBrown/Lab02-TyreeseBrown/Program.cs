6c877437daebebd4cd29a8d0d3014a9817bfb3fd
Tyreese Brown <tyreesebrown216@yahoo.com>
2018-02-10 20:15:36 -0500

    Lab 2

---
+++ Lab02-TyreeseBrown/Lab02-TyreeseBrown/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_TyreeseBrown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#2
+            Console.WriteLine("Enter an integer");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another integer");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + b);
+
+            
+            //#3
+            int x = 2;
+            int y = 3;
+            int z = 4;
+            Console.WriteLine((x + y) * (z + 10));
+
+            //#4
+            Console.WriteLine("Hello\tWorld!");
+            Console.WriteLine("Hello\nWorld!");
+            Console.WriteLine("\"Hello World!\"");
+            Console.WriteLine("Hello\\World!");
+        }
+    }
+}
--- Lab02-TyreeseBrown/Lab02-TyreeseBrown/Properties/AssemblyInfo.cs
+++ Lab02-TyreeseBrown/Lab02-TyreeseBrown/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab01")]
+[assembly: AssemblyTitle("Lab02-TyreeseBrown")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("Lab01")]
+[assembly: AssemblyProduct("Lab02-TyreeseBrown")]
 [assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("14e1c953-46b0-4a1c-8a5c-a0c5e52a7a2b")]
+[assembly: Guid("e3d657bf-74e7-4d5a-8eb7-d4069b5e62f2")]
 
 // Version information for an assembly consists of the following four values:
 //
