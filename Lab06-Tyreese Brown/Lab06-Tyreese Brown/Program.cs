dad316a6875207001f6c6515b8a9581e33f7f067
Tyreese Brown <tyreesebrown216@yahoo.com>
2018-03-05 00:21:56 -0500

    Lab06

---
+++ Lab06-Tyreese Brown/Lab06-Tyreese Brown/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_Tyreese_Brown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question 1
+            int counter = 0;
+            int year = 2018;
+            while (counter < 20)
+            {
+                if (year % 4 == 0)
+                {
+                    Console.WriteLine(year);
+                    counter++;
+                }
+                year++;
+            }
+
+            //Question 2
+            int secretNumber = 15;
+            int i = 1;
+            while (true)
+            {
+                Console.WriteLine("Enter a number between 1 and 100 to guess the secret number");
+                var input = Convert.ToInt32(Console.ReadLine());
+                if (input > secretNumber)
+                {
+                    Console.WriteLine("Your guess is too high. Try again.");
+                    ++i;
+                    continue;
+                }
+                else if (input < secretNumber)
+                {
+                    Console.WriteLine("Your guess is too low. Try again.");
+                    ++i;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed the right number! It was " + secretNumber);
+                    Console.WriteLine("It took you this many times to guess the right number: " + i);
+                }
+            }
+        }
+    }
+}
--- Lab06-Tyreese Brown/Lab06-Tyreese Brown/Properties/AssemblyInfo.cs
+++ Lab06-Tyreese Brown/Lab06-Tyreese Brown/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab02-TyreeseBrown")]
+[assembly: AssemblyTitle("Lab06-Tyreese Brown")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("Lab02-TyreeseBrown")]
+[assembly: AssemblyProduct("Lab06-Tyreese Brown")]
 [assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e3d657bf-74e7-4d5a-8eb7-d4069b5e62f2")]
+[assembly: Guid("4e1b3ecf-b183-4d7c-95a9-8576ba42067a")]
 
 // Version information for an assembly consists of the following four values:
 //
