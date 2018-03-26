025e1f0cf16dde79fac13a38f301c78a5574a964
Tyreese Brown <tyreese.csu@gmail.com>
2018-03-26 10:26:03 -0400

    Add files via upload

---
+++ Lab08-TyreeseBrown/Lab08-TyreeseBrown/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08_TyreeseBrown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //Question 1
+             string[] names = {"Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford"};
+             Console.WriteLine("The second element is " + names[1] + ". And the last element is " + names[names.Length-1]);
+
+             //Question 2
+             int n;
+             string[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+             for (n = 0; n <= months.Length-1; n++)
+                  Console.WriteLine(n + 1 + " " + months[n]);
+
+             //Question 3
+             string[] seasons = { "Spring", "Summer", "Autumn", "Winter" };
+             foreach (string element in seasons)
+             {
+                 Console.WriteLine(element);
+             }
+
+             //Question 4
+             int[] intArray = new int[1000];
+             Random random = new Random();
+             int randomNumber;
+ 
+            foreach (int element in intArray)
+            { 
+                randomNumber = random.Next(0, 100);
+                Console.WriteLine(randomNumber);
+            }
+
+            //Question 5
+            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+            int i = 0;
+            while (i <= names.Length-1)
+            {
+                Console.WriteLine(names[i]);
+                i++; 
+            } //changing names[0] to names[i] and iterating i
+            
+        }
+    }
+}
